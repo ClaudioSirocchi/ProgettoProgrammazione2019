28199bc629617fc8ddc6b1df57a5b03d9d015751
ClaudioSirocchi <s1082025@studenti.univpm.it>
2019-07-06 17:03:05 +0200

    Creazione modello iniziale Springboot

---
+++ gdpElaborationApplication/src/main/java/it/univpm/gdpElaborationApplication/GdpElaborationApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.gdpElaborationApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdpElaborationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdpElaborationApplication.class, args);
+	}
+
+}
