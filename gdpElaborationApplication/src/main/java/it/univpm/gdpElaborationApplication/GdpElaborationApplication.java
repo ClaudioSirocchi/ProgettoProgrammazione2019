54ca7faed39f21f8d5bd6babeba9f64fe23ed0e2
ClaudioSirocchi <s1082025@studenti.univpm.it>
2019-07-07 10:29:08 +0200

    Aggiunta selettore url main
    Aggiunto selettore che consente di inserire l'inserimento di un link che non sia quello di default in modo da rendere il software riutilizzabile quando verranno create nuove statistiche

--- gdpElaborationApplication/src/main/java/it/univpm/gdpElaborationApplication/GdpElaborationApplication.java
+++ gdpElaborationApplication/src/main/java/it/univpm/gdpElaborationApplication/GdpElaborationApplication.java
@@ -1,13 +1,37 @@
 package it.univpm.gdpElaborationApplication;
 
+import java.io.*;
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import it.univpm.gdpElaborationApplication.program.DownloadData;
 
 @SpringBootApplication
 public class GdpElaborationApplication {
 
-	public static void main(String[] args) {
+	
+
+	private static Scanner input;
+
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(GdpElaborationApplication.class, args);
+		DownloadData gdp=new DownloadData();
+		input = new Scanner(System.in);
+		BufferedReader urlinput = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Benvenuto nel programma di analisi dei GDP europei"
+				+ "\nDigita 1 se vuoi inserire un url oppupre 2 per l'url di default");
+		int selector=input.nextInt();
+		if(selector==1) {
+			System.out.println("inserisci l'url");
+			String selurl=urlinput.readLine();
+			gdp.DownloadUrlData(selurl);
+		}
+		else if(selector==2){
+			gdp.DownloadUrlData();
+		}
+
 	}
 
 }
