4248f7212f53101d26b4ba1581696b69a89dd37b
ClaudioSirocchi <s1082025@studenti.univpm.it>
2019-07-09 17:15:37 +0200

    Modifica Packages
    Rimosso package program e sostituito con package dataclass, aggiunta delle classi rilevazione e gdp per gestire i dati nel csv

---
+++ gdpElaborationApplication/src/main/java/it/univpm/gdpElaborationApplication/dataclass/GDP.java
@@ -0,0 +1,19 @@
+package it.univpm.gdpElaborationApplication.dataclass;
+
+public class GDP {
+	private int date;
+	private float value;
+	
+	public int getDate() {
+		return date;
+	}
+	public void setDate(int date) {
+		this.date = date;
+	}
+	public float getValue() {
+		return value;
+	}
+	public void setValue(float value) {
+		this.value = value;
+	}
+}
---
+++ gdpElaborationApplication/src/main/java/it/univpm/gdpElaborationApplication/dataclass/Rilevazione.java
@@ -0,0 +1,41 @@
+package it.univpm.gdpElaborationApplication.dataclass;
+
+public class Rilevazione {
+	private char frequenza;
+	private String geo;
+	private String unit;
+	private String obj;
+	private GDP gdp;
+	
+	public char getFrequenza() {
+		return frequenza;
+	}
+	public void setFrequenza(char frequenza) {
+		this.frequenza = frequenza;
+	}
+	public String getGeo() {
+		return geo;
+	}
+	public void setGeo(String geo) {
+		this.geo = geo;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	public String getObj() {
+		return obj;
+	}
+	public void setObj(String obj) {
+		this.obj = obj;
+	}
+	public GDP getGdp() {
+		return gdp;
+	}
+	public void setGdp(GDP gdp) {
+		this.gdp = gdp;
+	}
+	
+}
